7078925a954be38fa979bfae6782a47f0bf9ca0c
avandewauwer <annesophievandewauwer@gmail.com>
2021-07-02 10:14:03 +0200

    first push

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const taskController = require("./controllers/controller");
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
-app.listen(4000);
+
 
 app.post("/api/countries/add", (req, res) => {
   // console.log("From the brain I present:", req.body);
@@ -18,3 +18,5 @@ app.post("/api/removedCountry", (req, res) => {
 
   taskController.deleteInDatabase(req.body);
 });
+
+app.listen(process.env.PORT || 4000);
